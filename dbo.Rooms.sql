747deb1f4807c533fa878e295ea712d424367dcf
rein7703 <69892731+rein7703@users.noreply.github.com>
2021-04-10 01:43:55 +0700

    Add files via upload

---
+++ dbo.Rooms.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Rooms] (
+    [roomName]  VARCHAR (50) NOT NULL,
+    [roomPrice] BIGINT       NULL,
+    [isFilled]  VARCHAR (50) NULL,
+    [isPaid]    VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([roomName] ASC)
+);
+
