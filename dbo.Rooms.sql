b7eff6718dc49946d1f73baabfbd9a2e33ac91d7
rein7703 <69892731+rein7703@users.noreply.github.com>
2021-04-10 02:37:14 +0700

    Delete dbo.Rooms.sql

--- dbo.Rooms.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Rooms] (
-    [roomName]  VARCHAR (50) NOT NULL,
-    [roomPrice] BIGINT       NULL,
-    [isFilled]  VARCHAR (50) NULL,
-    [isPaid]    VARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([roomName] ASC)
-);
-
